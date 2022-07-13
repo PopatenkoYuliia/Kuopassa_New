deb42ef3a038770ea6400e11ecb70d9201288262
Juliia <yuliapopatenko94@gmail.com>
2022-07-13 23:43:25 +0300

    first commit

---
+++ src/main/java/pages/ShoppingCartPage.java
@@ -0,0 +1,21 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ShoppingCartPage extends BasePage{
+
+    @FindBy(xpath = "//*[@id='box-checkout-cart']/div")
+    private WebElement tableResponsive;
+
+    public ShoppingCartPage(WebDriver driver) {
+        super(driver);
+    }
+    public String getTextItem(){
+        waitVisibilityOfElement(60,tableResponsive);
+        String itemText=tableResponsive.getText();
+        return itemText;
+    }
+
+}
